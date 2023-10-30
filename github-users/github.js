3e868793ca13fbb0d475f52e54e9383dbb378bab
Shahzeb Waleed <135195892+waleeddcoder@users.noreply.github.com>
2023-10-30 17:10:40 +0500

    Add files via upload

--- github-users/github.js
+++ github-users/github.js
@@ -1,36 +1,70 @@
 const APIURL = "https://api.github.com/users/";
 const main = document.querySelector("#main");
 const searchBox = document.querySelector("#search");
-const getUser = async (username) => {
-    const response = await fetch(APIURL + username);
-    const data = await response.json();
-    const card = `
-        <div class="card">
-            <div>
-                <img class="avatar" src="${data.avatar_url}" alt="Florin Pop">
-            </div>
-            <div class="user-info">
-                <h2>${data.name}</h2>
-                <p>${data.bio}</p>
-
-                <ul class="info">
-                    <li>${data.followers}<strong>Followers</strong></li>
-                    <li>${data.following}<strong>Following</strong></li>
-                    <li>${data.public_repos}<strong>Repos</strong></li>
-                </ul>
-
-                <div id="repos">
-                  
-                </div>
-            </div>
-        </div>
-    `;
-    main.innerHTML = card;
-    getRepos(username);
+const loading = document.querySelector("#loading");
+const gear = document.querySelector(".gearforgithub");
+const showGear = () => {
+    gear.style.display = "block";
 };
 
-// init call
+const hideGear = () => {
+    gear.style.display = "none";
+};
+const getUser = async (username) => {
+    loading.style.display = "block";
+    try {
+        const response = await fetch(APIURL + username);
+        if (response.status === 404) {
+            showUserNotFound();
+            hideGear();
+        } else {
+            const data = await response.json();
+            const card = `
+                <div class="card">
+                    <div>
+                        <img class="avatar" src="${data.avatar_url}" alt="Florin Pop">
+                    </div>
+                    <div class="user-info">
+                        <h2>${data.name}</h2>
+                        <p>${data.bio}</p>
+        
+                        <ul class="info">
+                            <li>${data.followers}<strong>Followers</strong></li>
+                            <li>${data.following}<strong>Following</strong></li>
+                            <li>${data.public_repos}<strong>Repos</strong></li>
+                        </ul>
+        
+                        <div id="repos">
+                          
+                        </div>
+                    </div>
+                </div>
+            `;
+            main.innerHTML = card;
+            getRepos(username);
+            showGear();
+        }
+    } catch (error) {
+        showUserNotFound();
+        hideGear();
+    }
+    loading.style.display = "none";
+};
+
+loading.style.display = "block";
 getUser("waleeddcoder");
+
+const showUserNotFound = () => {
+    const oopsImage = document.createElement("img");
+    oopsImage.src = "assets/404.png";
+    oopsImage.alt = "User not found";
+    oopsImage.style.maxWidth = "100%"; // Limit the image width to the container's width
+    oopsImage.style.height = "auto"; // Maintain aspect ratio
+    oopsImage.style.margin = "0 auto"; // Center the image horizontally
+    oopsImage.style.display = "block"; // Ensure it's a block element
+    main.innerHTML = "";
+    main.appendChild(oopsImage);
+};
 
 const getRepos = async (username) => {
     const repos = document.querySelector("#repos");
